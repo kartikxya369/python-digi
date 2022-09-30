77786c8fe261c757cfccc773a69a8126a37b5072
kartikxya369 <kartikeyatiwari316@gmail.com>
2022-09-30 16:03:35 +0530

    first push

---
+++ strings/s1.py
@@ -0,0 +1,15 @@
+my_string='Hello'
+print(my_string)
+
+my_string= "Hello"
+print(my_string)
+
+my_string= """Hello"""
+print(my_string)
+
+my_string = """Hello, welcome to the world of python."""
+print(my_string)
+
+name = "Leonardo"
+print(name[0])
+print(name[1])
---
+++ t1.py
@@ -0,0 +1,14 @@
+# pentagon
+from turtle import *
+for i in range(5):
+    pensize (10)
+    pencolor('violet')
+    fd(150)
+    lt(72)
+    dot(5*5)
+    pensize(20)
+    pencolor('violet')
+    circle(5*10)
+
+
+mainloop()
