c4522e3b3bc30d8afcc2e807e7410c4225c84a0e
kartikxya369 <kartikeyatiwari316@gmail.com>
2022-09-30 16:47:38 +0530

    string update 2

--- t1.py
+++ t1.py
@@ -1,14 +1,1 @@
-# pentagon
-from turtle import *
-for i in range(5):
-    pensize (10)
-    pencolor('violet')
-    fd(150)
-    lt(72)
-    dot(5*5)
-    pensize(20)
-    pencolor('violet')
-    circle(5*10)
-
-
-mainloop()
+from turtle import*
